c0555c43b09dadb4155e1266f1426556645f05ed
Rodolfo Moreira <rodolformoreira@gmail.com>
2019-08-14 23:15:40 -0300

    teste

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask
-
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return 1
+@app.route("/")
+def hello():
+    return "Hello World!"
 
-app.run(debug = True)
+if __name__ == "__main__":
+    app.run()
